8d36d862518269a654e629865238de975eb575c1
emanuelboboiu <emanuelboboiu@gmail.com>
2025-07-13 13:42:51 +0300

    Version 9.7, April 2025.

--- app/src/main/java/ro/pontes/culturagenerala/DataBaseHelper.java
+++ app/src/main/java/ro/pontes/culturagenerala/DataBaseHelper.java
@@ -21,8 +21,8 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     private static String DB_PATH = "";
     private static final String DB_NAME = "cultura_generala.db";
     private static final String SP_KEY_DB_VER = "dbVer";
-    private static final int DATABASE_VERSION = 41;
-    private static final int lastUpdateTimestamp = 1722189600; // 28 July 2024.
+    private static final int DATABASE_VERSION = 47;
+    private static final int lastUpdateTimestamp = 1744416000; // 12 April 2025.
 
     private SQLiteDatabase mDataBase;
     private final Context mContext;
